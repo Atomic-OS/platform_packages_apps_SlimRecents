AICP/frameworks_opt_slimrecent

Improve slimrecents toggling reliability
Thanks to ezio84 for working together with me here once again

Change-Id: Ia914dfa6ac3bc8e9f37900571cb685e39e12a697

src/com/android/systemui/slimrecent/RecentController.java
@@ -479,7 +479,7 @@ public class RecentController implements RecentPanelView.OnExitListener,
         if (mAnimationState == ANIMATION_STATE_NONE) {
             if (!isShowing()) {
                 mIsToggled = true;
-                if (mRecentPanelView.isTasksLoaded()) {
+                if (mRecentPanelView.areAllTasksLoaded()) {
                     showRecents();
                 } else if (!mIsPreloaded) {
                     // This should never happen due that preload should

Atomic-OS/platform_packages_apps_SlimRecents

Change-Id: Icaf4210213c2242fd0f6f94537d244efca838851

src/com/android/systemui/slimrecent/RecentController.java
@@ -424,7 +424,7 @@ public class RecentController implements RecentPanelView.OnExitListener,
         if (mAnimationState == ANIMATION_STATE_NONE) {
             if (!isShowing()) {
                 mIsToggled = true;
-                if (mRecentPanelView.isTasksLoaded()) {
+                if (mRecentPanelView.atLeastOneTaskAvailable()) {
                     showRecents();
                 } else if (!mIsPreloaded) {
                     // This should never happen due that preload should